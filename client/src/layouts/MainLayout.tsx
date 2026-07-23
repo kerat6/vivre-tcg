29812a4926866eb426e6a1474d6cf9488de7ee45
Tarek Al-Ahmadieh <t.ahmadieh@outlook.com>
2026-07-23 18:13:28 -0400

    Add background color for visuals

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet, Link } from 'react-router-dom'
 
 function MainLayout() {
   return (
-  <div>
+  <div className="min-h-screen bg-gray-100">
     <nav className="flex gap-4 p-4 border-b bg-white">
         <Link to="/" className="font-semibold text-gray-700 hover:text-black">Browse Cards</Link>
         <Link to="/deck-builder" className="font-semibold text-gray-700 hover:text-black">Deckbuilder</Link>
